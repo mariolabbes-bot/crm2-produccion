e971cdd770e974b207cd2aaf66a1964d55c1000e
Mario Labbe <mariolabbe@Marios-MacBook-Air.local>
2025-10-07 14:09:31 -0300

    Fix webpack build for production - add HtmlWebpackPlugin and optimize bundle

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,39 +1,67 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true,
-  },
-  devServer: {
-    static: './public',
-    historyApiFallback: true,
-    port: 3000,
-    open: true,
-    host: 'localhost',
-    allowedHosts: 'all'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProduction ? 'bundle.[contenthash].js' : 'bundle.js',
+      clean: true,
+      publicPath: '/',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        inject: true,
+      }),
+    ],
+    devServer: {
+      static: './public',
+      historyApiFallback: true,
+      port: 3000,
+      open: true,
+      host: 'localhost',
+      allowedHosts: 'all'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    optimization: isProduction ? {
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
           },
         },
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
+    } : {},
+    performance: {
+      maxAssetSize: 1000000,
+      maxEntrypointSize: 1000000,
+    },
+  };
 };
